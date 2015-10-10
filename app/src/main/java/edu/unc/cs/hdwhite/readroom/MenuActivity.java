f533f0c10f36ed6153372f381d15f3fd49854307
mattbent <mattfranklinbent@gmail.com>
2015-10-10 12:01:23 -0400

    So far not working, gui intact in IDE though

--- app/src/main/java/edu/unc/cs/hdwhite/readroom/MenuActivity.java
+++ app/src/main/java/edu/unc/cs/hdwhite/readroom/MenuActivity.java
@@ -1,12 +1,15 @@
 package edu.unc.cs.hdwhite.readroom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MenuActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,5 +36,13 @@ public class MenuActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void sendMesssage(View v)
+    {
+        Intent intent = new Intent(this, DisplayResults.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
