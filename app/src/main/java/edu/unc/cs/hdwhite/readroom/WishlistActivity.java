f19c8c7347f720bc7dd8f24ff534c92230b785e7
hdwhite <hughdwhite@gmail.com>
2015-10-10 20:55:32 -0400

    added book passing between activities

--- app/src/main/java/edu/unc/cs/hdwhite/readroom/WishlistActivity.java
+++ app/src/main/java/edu/unc/cs/hdwhite/readroom/WishlistActivity.java
@@ -24,6 +24,11 @@ public class WishlistActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        addBooks((ArrayList<Book>) getIntent().getSerializableExtra("books"));
+    }
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_wishlist, menu);
